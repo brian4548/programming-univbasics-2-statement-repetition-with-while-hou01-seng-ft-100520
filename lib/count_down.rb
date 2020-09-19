f409c756cb1b254440e41853cb6681b553df1e26
Brian Rhodes <brianr4548@gmail.com>
2020-09-19 06:25:19 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,14 @@
 # Write your code here
+
+end_number = 0 
+count = 10
+
+while count = 1 do
+  puts "Happy_New_Year"
+end 
+
+while count < 11 && count != end_number do
+  puts "#{count}"
+  count -= 1
+end
+
